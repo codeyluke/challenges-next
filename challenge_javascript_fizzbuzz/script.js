5063208df1b82a91efb81aa3a560db7cc33c260c
Luke Sia <lukekennedy212@gmail.com>
2018-12-04 22:46:06 +0800

    Javascript fizzbuzz

---
+++ challenge_javascript_fizzbuzz/script.js
@@ -0,0 +1,20 @@
+function doFizzBuzz() {
+  const num = prompt("Please enter your number");
+  if (num % 3 == 0 && num % 5 == 0) {
+    document.getElementById(
+      "result"
+    ).innerHTML = `<h3 class="container text-center m-2">FizzBuzz</h3>`;
+  } else if (num % 5 == 0) {
+    document.getElementById(
+      "result"
+    ).innerHTML = `<h3 class="container text-center m-2">Buzz</h3>`;
+  } else if (num % 3 == 0) {
+    document.getElementById(
+      "result"
+    ).innerHTML = `<h3 class="container text-center m-2">Fizz</h3>`;
+  } else {
+    document.getElementById(
+      "result"
+    ).innerHTML = `<h3 class="container text-center m-2">The number ${num} is not a multiple of 3 and 5.</h3>`;
+  }
+}
