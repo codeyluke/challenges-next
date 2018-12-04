efa3cab9aa4ae0cdbd90325bcbf3e77f1cff8c93
Luke Sia <lukekennedy212@gmail.com>
2018-12-04 17:50:21 +0800

    third

--- challenge_javascript_randomize/script.js
+++ challenge_javascript_randomize/script.js
@@ -8,3 +8,21 @@ const quotes = [
 
 const randomQuotes = Math.floor(Math.random() * quotes.length);
 document.write(`<h3 class="container">${quotes[randomQuotes]}</h3>`);
+
+const pets = ["dogs", "cats", "mice", "rabbits", "hamsters"];
+
+function pickRandomFromArray(myArray, start, end) {
+  if (end > myArray.length) {
+    return `Please enter a value lower than ${myArray.length}`;
+  } else {
+    const newArray = myArray.slice(start, end);
+    const randomPick = Math.floor(Math.random() * newArray.length);
+    const picked = newArray[randomPick];
+    return picked;
+  }
+}
+
+const guessPet = pickRandomFromArray(pets, 0, 4);
+document.write(
+  `<h3 class="container">The random animal would be ${guessPet}.</h3>`
+);
